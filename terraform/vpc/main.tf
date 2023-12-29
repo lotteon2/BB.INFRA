1510b5c4c8e943f8eba5e609f32f923342172abe
nowgnas <dev.nowgnas@gmail.com>
2023-12-29 09:04:53 +0900

    :wrench: Amend store ddl

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -2,51 +2,31 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-locals {
-  cluster_name = "blooming-blooms"
-}
-
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = local.cluster_name
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-northeast-2a", "ap-northeast-2b"]
-  public_subnets  = ["10.0.10.0/24", "10.0.20.0/24"]
-  private_subnets = ["10.0.11.0/24", "10.0.21.0/24"]
-
-  enable_nat_gateway     = true
-  one_nat_gateway_per_az = true
-
+  name                 = "blooming-blooms"
+  cidr                 = "10.0.0.0/16"
+  azs                  = ["ap-northeast-2a", "ap-northeast-2b"]
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets       = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_support   = true
   enable_dns_hostnames = true
-
-  tags = {
-    Terraform   = "true"
-    Environment = "prod"
-  }
-
 }
 
-# Create Security Group for EKS
-resource "aws_security_group" "eks_security_group" {
-  name        = "eks-security-group"
-  description = "Security group for BB EKS"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    description = "Allow inbound traffic for BB EKS"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#resource "aws_route_table_association" "bastion_association" {
+#  subnet_id      = module.vpc.public_subnets[0]
+#  route_table_id = module.vpc.public_subnet_route_table_ids[0]
+#}
+#
+#resource "aws_route_table_association" "nat_association_eks_a" {
+#  subnet_id      = module.vpc.private_subnets[0]
+#  route_table_id = module.vpc.private_subnet_route_table_ids[0]
+#}
+#
+#resource "aws_route_table_association" "nat_association_eks_b" {
+#  subnet_id      = module.vpc.private_subnets[1]
+#  route_table_id = module.vpc.private_subnet_route_table_ids[1]
+#}
